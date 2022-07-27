1cfce69d90af6d545c31fdf8c715076a51800f18
monika <monika>
2022-07-27 09:59:00 -0700

    freelencer form complete

---
+++ main.js
@@ -0,0 +1,18 @@
+function myFunction() {
+    var x = document.getElementById("inputVlaueUser");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  } 
+
+
+  function myFunctioncomfirm() {
+    var x = document.getElementById("inputVlaueUserComfirm");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  } 
